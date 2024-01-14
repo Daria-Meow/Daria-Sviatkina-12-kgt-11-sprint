817df03e5371dde33af48f6f0a8d30dff34d47a7
Daria <dsviatkina@yandex.ru>
2024-01-14 13:24:51 +0700

    Добавлен запрос для задания 2

--- sql_requests.sql
+++ sql_requests.sql
@@ -7,3 +7,9 @@ FROM
   AND o."inDelivery" = 't'
 GROUP BY
   c.login;
+
+SELECT
+  track,
+  CASE WHEN finished = 't' THEN 2 WHEN cancelled = 't' THEN -1 WHEN "inDelivery" = 't' THEN 1 ELSE 0 END AS status
+FROM
+  "Orders";
