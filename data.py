41c2959602b7b3a13db611325ffea3fb4dac4f84
Daria <dsviatkina@yandex.ru>
2024-01-14 14:25:03 +0700

    Добавлен файл data.py c данными для запросов

---
+++ data.py
@@ -0,0 +1,17 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+order_body = {
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "mymy, 142 apt.",
+    "metroStation": 5,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2020-06-06",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
